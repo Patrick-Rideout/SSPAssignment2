967f203d657d26bf8606e4071c6cfb9174000cd1
Patrick Rideout <123347247+Patrick-Rideout@users.noreply.github.com>
2025-02-24 14:18:22 -0330

    Delete SSP A2 directory

--- SSP A2/myapp/routes/register.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const registerRouter = express.Router();
-
-const { registerStudentToCourse } = require("../controllers/register.controller.js");
-
-registerRouter.post("/", registerStudentToCourse);
-
-module.exports = registerRouter;
